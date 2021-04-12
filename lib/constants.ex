f9dffed382064e134effd667f315903b2d60e815
Daniel Bjerkehagen <danielbb@stud.ntnu.no>
2021-04-12 14:40:01 +0200

    Begun ping-func, added RuntimeConst, cleanup

--- lib/constants.ex
+++ lib/constants.ex
@@ -1,12 +1,12 @@
 defmodule Constants do
-  def number_of_elevators, do: 2
+  def number_of_elevators, do: 3
 
   def number_of_floors, do: 3
 
-  def elevator_ip_to_string do
+  def get_elevator_ip_string do # This is not really constant
     {:ok, ip_info} = :inet.getif
     case Enum.at(ip_info,0) do  # This should not be an enum.at
-      {my_ip, _router, {255, 255, 252, 0}} -> Enum.join(Tuple.to_list(my_ip), ".")
+      {my_ip, _router, {255, 255, _255_or_252, 0}} -> Enum.join(Tuple.to_list(my_ip), ".")
       {:error, _error_msg} -> :failed_to_get_ip
     end
   end
@@ -24,4 +24,18 @@ defmodule Constants do
   def driver_wait_loop_sleep_time, do: 100
 
   def ping_wait_time_ms, do: 1000
+end
+
+
+
+defmodule RuntimeConstants do
+  use Agent
+
+  def start_link(elev_number) do
+    Agent.start_link(fn -> elev_number end, name: __MODULE__)
+  end
+
+  def get_elev_number do
+    Agent.get(__MODULE__, & &1)
+  end
 end
