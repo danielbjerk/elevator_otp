833a8c03d410370cf16c250c5d1a48258168daa2
Daniel Bjerkehagen <danielbb@stud.ntnu.no>
2021-04-15 11:51:57 +0200

    Working OrderLogger without watchdog

--- lib/queue.ex
+++ lib/queue.ex
@@ -100,3 +100,76 @@ defmodule Queue do  # TODO: Gå gjennom og fjern alle unødvendige funksjone
   end
 
 end
+
+
+
+defmodule OrderLogger do
+  use Agent
+
+  def start_link(my_elev_number) do
+    {:ok, _agent} = Agent.start_link(__MODULE__, :generate_empty_logger, [my_elev_number], name: __MODULE__)
+  end
+
+  def generate_empty_logger(my_elev_number) do
+    Map.new(Peer.list_all_node_names_except([my_elev_number]), fn name -> {name, generate_empty_queue} end)
+  end
+  def generate_empty_queue do
+    Enum.map(Constants.all_floors_range, fn floor -> [{floor, :hall_up, :no_order}, {floor, :hall_down, :no_order}, {floor, :cab, :no_order}] end)
+  end
+
+
+
+  # Accessing and modifying the queue
+
+  def get_queue_of_node(node) do
+    Agent.get(__MODULE__, fn log -> log[node] end)
+  end
+
+  def add_order(to_node, order) do
+    Agent.update(__MODULE__, __MODULE__, :update_order_in_logger, [to_node, order])
+  end
+
+  def remove_all_orders_to_floor(of_node, floor) do
+    Agent.update(__MODULE__, __MODULE__, :update_order_in_logger, [of_node, {floor, :hall_up, :no_order}])
+    Agent.update(__MODULE__, __MODULE__, :update_order_in_logger, [of_node, {floor, :hall_down, :no_order}])
+    Agent.update(__MODULE__, __MODULE__, :update_order_in_logger, [of_node, {floor, :cab, :no_order}])
+  end
+
+  def update_order_in_logger(logger, node, order) do
+    {floor, order_type, _order_here?} = order
+
+    queue = logger[node]
+    orders_at_floor = Enum.at(queue, floor)
+
+    orders_at_floor_updated = List.replace_at(orders_at_floor, order_type_to_queue_index(order_type), order)
+    queue_updated = List.replace_at(queue, floor, orders_at_floor_updated)
+    Map.put(logger, node, queue_updated)
+  end
+  
+
+  
+  # Helper functions
+
+  def get_all_active_orders(node) do
+    List.flatten(Enum.map(Constants.all_floors_range, fn floor -> get_all_active_orders_at_floor(node, floor) end))
+  end
+
+  def get_all_active_orders_at_floor(node, floor) do
+    orders_at_floor = Agent.get(__MODULE__, fn logger -> Enum.at(logger[node], floor) end)
+    Enum.filter(orders_at_floor, fn order ->
+      case order do
+        {_floor, _order_type, :order} -> true
+        _ -> false
+      end
+    end)
+  end
+
+  def order_type_to_queue_index(order_type) do
+    case order_type do
+      :hall_up -> 0
+      :hall_down -> 1
+      :cab -> 2
+      _ -> :not_a_order_type
+    end
+  end
+end
