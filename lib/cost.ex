030f220b5a002e14a1f269740d8b5795ff8c494e
Chris Vaksdal <chris.vaksdal@gmail.com>
2021-04-17 15:29:25 +0200

    Fixed cost-function

--- lib/cost.ex
+++ lib/cost.ex
@@ -2,19 +2,23 @@ defmodule Cost do
 
     def add_distance_cost(cost, position, order) do # Add cost due to distance between current position and order position.
         {elevator_floor, _elevator_direction} = position
-        {order_floor, _order_direction} = order
+        {order_floor, _order_direction, :order} = order
         cost + Kernel.abs(order_floor - elevator_floor) * 2
     end
 
     def add_queue_length_cost(cost, q) do   # Add cost due to current length of queue
-        cost + tuple_size(q) * 1
+        cost + length(q) * 1
+    end
+
+    def no_cost() do
+        -10000
     end
 
 
     def add_direction_cost(cost, order) do  # Add cost due to direction of order. Can result in :no_cost.
-        {order_floor, order_direction} = order
+        {order_floor, order_direction, :order} = order
         if Queue.order_compatible_with_direction_at_floor?(order_floor, order_direction) do
-            :no_cost
+            no_cost()
         else
             cost + 10
         end
@@ -25,10 +29,10 @@ defmodule Cost do
         try do
 
             pTask = Task.async(fn -> Position.get() end)
-            position = Task.await(pTask, timeout: 100)
+            position = Task.await(pTask, 100)
 
             qTask = Task.async(fn -> Queue.get() end)
-            q = Task.await(qTask, timeout: 100)
+            q = Task.await(qTask, 100)
 
             add_distance_cost(0, position, order)
             |> add_queue_length_cost(q)
@@ -40,9 +44,11 @@ defmodule Cost do
             :timeout -> :infinite_cost    # Failed to retrieve position for elevator.
 
             e ->
-                IO.puts("Something went wrong")
+                IO.puts("Something went wrong. Error:")
                 IO.inspect(e)
+                IO.puts("End-error")
                 :error
+                
 
         end
 
